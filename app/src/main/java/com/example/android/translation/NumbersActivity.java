bd632a5d57efb4dad1df7d8db4717519d0c6ba45
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-01-17 15:54:39 +0000

    create an array of english words

--- app/src/main/java/com/example/android/translation/NumbersActivity.java
+++ app/src/main/java/com/example/android/translation/NumbersActivity.java
@@ -9,5 +9,18 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Create an array of words
+        String[] englishWords = new String[10];
+        englishWords[0] = "one";
+        englishWords[1] = "two";
+        englishWords[2] = "three";
+        englishWords[3] = "four";
+        englishWords[4] = "five";
+        englishWords[5] = "six";
+        englishWords[6] = "seven";
+        englishWords[7] = "eight";
+        englishWords[8] = "nine";
+        englishWords[9] = "ten";
     }
 }
