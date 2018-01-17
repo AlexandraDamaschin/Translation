d0f611db19c35f3cefa1a48c73cad361737222a8
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-01-17 11:51:41 +0000

    explicit intent

--- app/src/main/java/com/example/android/translation/MainActivity.java
+++ app/src/main/java/com/example/android/translation/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //explicit intent
     public void openNumbersList(View view) {
         Intent i = new Intent(this, NumbersActivity.class);
         startActivity(i);
