e9f9be0420a112975a78b6df2516d2ff60ca085f
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-01-20 18:49:55 +0000

    ro translation working

---
+++ app/src/main/java/com/example/android/translation/WordAdapter.java
@@ -0,0 +1,40 @@
+package com.example.android.translation;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by e6420 on 1/20/2018.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    public WordAdapter(Context context, ArrayList<Word> words) {
+        super(context, 0, words);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if an existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+
+        TextView romanianTextView = (TextView) listItemView.findViewById(R.id.romanian_text_view);
+        romanianTextView.setText(currentWord.getmRomanianTranslation());
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        return listItemView;
+    }
+}
