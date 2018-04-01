e86c46d496a3521af54717b9669ab1984ea83270
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-04-01 20:20:02 +0100

    ac4060f..025a734  master -> master

--- app/src/main/java/com/example/android/translation/NumbersActivity.java
+++ app/src/main/java/com/example/android/translation/NumbersActivity.java
@@ -10,10 +10,12 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //displays the activity_category.xml layout
         setContentView(R.layout.activity_category);
         //replace activity with the container created
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container, new NumbersFragment())
+                //NumbersFragment displays the word_list.xml layout
                 .commit();
     }
 }
