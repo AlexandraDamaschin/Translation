a0add933dde29c0c3bf0f01b7d0a1c61ebbdc2a8
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-04-01 21:08:01 +0100

    phrases fragment

--- app/src/main/java/com/example/android/translation/PhrasesActivity.java
+++ app/src/main/java/com/example/android/translation/PhrasesActivity.java
@@ -7,29 +7,15 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
-
-        // Create a list of words
-        ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("Where are you going?", "Unde esti?", R.raw.unu ));
-        words.add(new Word("What is your name?", "Cum te numesti?", R.raw.unu ));
-        words.add(new Word("My name is...", "Numele meu eate ...", R.raw.unu ));
-        words.add(new Word("How are you feeling?", "Cuum te simti?", R.raw.unu ));
-        words.add(new Word("I’m feeling good.", "Ma simt bine.", R.raw.unu ));
-        words.add(new Word("Are you coming?", "Vi?", R.raw.unu ));
-        words.add(new Word("Yes, I’m coming.", "Da, vin.", R.raw.unu ));
-        words.add(new Word("I’m coming.", "Vin", R.raw.unu ));
-        words.add(new Word("Let’s go.", "Sa mergem.", R.raw.unu ));
-        words.add(new Word("Come here.", "Vino aici,", R.raw.unu ));
-
-        WordAdapter adapter = new WordAdapter(this, words ,R.color.category_phrases);
-
-        ListView listView = (ListView) findViewById(R.id.list);
-
-        listView.setAdapter(adapter);
+        //displays the activity_category.xml layout
+        setContentView(R.layout.activity_category);
+        //replace activity with the container created
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new PhrasesFragment())
+                //PhrasesFragment displays the word_list.xml layout
+                .commit();
     }
 }
